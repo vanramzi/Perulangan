4e42cff1f461c183e379ea5299aed3477652485a
vanramzi <ramzievan3@gmail.com>
2024-10-21 11:22:30 +0700

    Add files via upload

---
+++ NomorEmpat.java
@@ -0,0 +1,11 @@
+public class NomorEmpat {
+    public static void main(String[] args) {
+        int x =1 , a=0;
+        while (x<=121) {
+            System.out.println(x + "");
+            x = + a;
+            a = a+1;
+        }
+    }
+    
+}
---
+++ NomorLima.java
@@ -0,0 +1,20 @@
+public class NomorLima {
+    public static void main(String[] args) {
+        int n1 =0, n2=1, n3;
+        int count = 16;
+        int i = 2;
+        System.out.println(n1+ "" + n2);
+
+        while (i < count) {
+            n3 = n1+n2;
+            System.out.println( "" + n3);
+            n1 = n2;
+            n2 = n3;
+            i++;
+            
+        }
+    }
+    
+}
+
+    
